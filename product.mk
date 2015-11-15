d909e9ab4154a5e952f3db9321379651d445aafd
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-15 07:39:54 -0600

    extra: set twrp as recovery variant
    * also set ro.secure=0 and ro.adb.secure=0
    
    Change-Id: Ib53c99b739e59cef37d70084455e451ce0e37f1c

--- product.mk
+++ product.mk
@@ -1,3 +1,7 @@
+RECOVERY_VARIANT := twrp
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 
 $(call prepend-product-if-exists, vendor/invisiblek/product.mk)
