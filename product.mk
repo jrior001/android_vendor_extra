3cb3b495231c475311ee46236f3db460f7d98254
Dan Pasanen <dan.pasanen@gmail.com>
2017-10-13 08:12:02 -0500

    extra: enable sdcardfs for everyone

--- product.mk
+++ product.mk
@@ -1,4 +1,7 @@
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 PRODUCT_PACKAGES += auditd
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
 $(call prepend-product-if-exists, vendor/invisiblek/product.mk)
