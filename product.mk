4b05feaf2c862ee1b88ea21cc1473e4bbfad5268
jrior001 <jriordan001@gmail.com>
2017-12-26 19:11:10 -0500

    updater: use my local updater

--- product.mk
+++ product.mk
@@ -1,4 +1,7 @@
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 PRODUCT_PACKAGES += auditd
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    cm.updater.uri=http://updater.theflamingskull.com/api
+
 $(call prepend-product-if-exists, vendor/jrior001/product.mk)
