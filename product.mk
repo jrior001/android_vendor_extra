a55a7addec8d2dbefd177852c0369438b4f85f63
jrior001 <jriordan001@gmail.com>
2018-07-15 14:11:31 -0400

    Revert "Add pixel theme"
    This reverts commit 75ba1edfa05b0701ab53ebf48012425d5a3c8c57.
    
    Change-Id: Idbad25537c8e513bcc526d3e1f20bf1bc66d0d86

--- product.mk
+++ product.mk
@@ -22,10 +22,3 @@ TARGET_EXTRA_KERNEL_MODULES := wireguard
 
 PRODUCT_COPY_FILES += \
     vendor/extra/rootdir/etc/init/ttl.rc:system/etc/init/ttl.rc
-
-# Themes
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-PRODUCT_PROPERTY_OVERRIDES += ro.boot.vendor.overlay.theme=com.google.android.theme.pixel
-PRODUCT_PROPERTY_OVERRIDES += ro.com.google.ime.theme_id=5
-PRODUCT_PACKAGES += \
-    PixelTheme
